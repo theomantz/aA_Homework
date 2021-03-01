40079cca31a02bd50e7ea36ab2387e1baec277f0
theo m <theo@mantz.io>
2021-03-01 09:39:49 -0500

    Update commit

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
 
     validates :password_digest, presence: { message: "Password cannot be blank " }
     validates :password, length: {minimum: 6, allow_nil: true}
+    validates :username, :session_token, presence: true
 
-    before_validation :password
+    before_validation :password, :ensure_session_token
 
     def self.find_by_credentials(username, password)
         @user = User.find_by(username: username )
@@ -19,7 +20,6 @@ class User < ApplicationRecord
         SecureRandom::urlsafe_base64
     end
 
-    # validates :username, :session_token, presence: true
 
     def reset_session_token!
         self.session_token = self.class.generate_session_token
@@ -36,7 +36,13 @@ class User < ApplicationRecord
         BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
-    def ensure_session_token
-        @user.reset_session_token! if @user.session_token.nil?
+    def session_token_nil?
+        session_token == nil 
     end
+
+    def ensure_session_token
+        reset_session_token! if session_token_nil?
+        session_token
+    end
+
 end

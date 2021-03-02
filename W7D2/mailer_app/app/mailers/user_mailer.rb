51b09f48fc021d08655b6824ae5f169f3f5a83f5
theo m <theo@mantz.io>
2021-03-02 11:04:28 -0500

    Update commit

---
+++ W7D2/mailer_app/app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+    default from: "notifications@example.com"
+
+    def welcome_email(user)
+        @user = user
+        @url = 'http://example.com/login'
+        mail(to: user.email, subject: 'Welcome to My Awesome Application')
+    end
+
+    def reminder_email(user)
+        # ...
+    end
+
+    
+end

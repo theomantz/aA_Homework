4dbaf671fed05636bf7307c5b97314b24a174156
theo m <theo@mantz.io>
2021-03-12 12:06:35 -0500

    W8D5 homework finish callbacks, initialize classes

--- W8D5/callsbacks_etc/some_tea_some_biscuits.js
+++ W8D5/callsbacks_etc/some_tea_some_biscuits.js
@@ -1,22 +1,29 @@
 // Require readLine Library
-
 const readline = require('readline');
 
 const reader = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
 // Tea and Biscuits
 function teaAndBiscuits() {
-    let first;
 
-    reader.question('Would you like some tea?', (res) => {
-        first = res;
+    reader.question('Would you like some tea? ', (first) => {
         console.log(`You replied ${first}.`);
+
+        reader.question('Would you like some biscuits? ', (second) => {
+            console.log(`You replied ${second}`);
+            console.log(`You replied ${first} then ${second}`)
+            
+            let reply_1 = first === 'yes' ? 'do' : 'don\'t'
+            let reply_2 = second === 'yes' ? 'do' : 'don\'t'
+            
+            console.log(`So you ${reply_1} want tea and you ${reply_2} want biscuits.`)
+            reader.close();
+        });
     });
 
-    reader.close();
 }
 
 teaAndBiscuits();
